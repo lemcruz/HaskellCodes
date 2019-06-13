d25dec471488c5db2f86cb7134d0e9ab73473ff4
unknown <lucaseustaquio25@gmail.com>
2019-06-13 18:27:59 -0300

    Changed sumOddNumbs file

--- sumOfOddNumbers.hs
+++ sumOfOddNumbers.hs
@@ -1,8 +1,12 @@
-fib :: Integer -> Integer
-fib 0 = 0
-fib 1 = 1
-fib n = fib (n-1) + fib(n - 2)
+sumOfOddNumbs :: [Int] -> Int
+sumOfOddNumbs ( a : x )
+ | (a : x ) == [] = 0
+ | a mod 2 == 0 = 0
+ | otherwise = a + sumOfOddNumbs x
 
 main = do
-  print $ fib (13)
+  print $ sumOfOddNumbs [1,2,3]
+ 
+  
+
 --Not working yet
