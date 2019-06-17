8ffb984852e4d7ff9b6fc56f18d99fd4296b463a
Lucas Eustaquio <lucaseustaquio25@gmail.com>
2019-06-17 00:02:14 -0300

    sumOfNumbers done and tested.

--- sumOfOddNumbers.hs
+++ sumOfOddNumbers.hs
@@ -1,12 +1,20 @@
 sumOfOddNumbs :: [Int] -> Int
-sumOfOddNumbs ( a : x )
- | (a : x ) == [] = 0
- | a mod 2 == 0 = 0
- | otherwise = a + sumOfOddNumbs x
+sumOfOddNumbs [] = 0
+sumOfOddNumbs (a:x) = 
+  if((mod a 2) == 0) 
+    then  0+sumOfOddNumbs x
+  else  a + sumOfOddNumbs x
+
+sumOfOddNumbsVer2 :: [Int] -> Int
+sumOfOddNumbsVer2 [] = 0
+sumOfOddNumbsVer2 (a:x)
+ | (mod a 2) /= 0 = a + sumOfOddNumbsVer2 x
+ | (mod a 2) == 0 = 0 + sumOfOddNumbsVer2 x
+
 
 main = do
-  print $ sumOfOddNumbs [1,2,3]
- 
+  let lista = [1,1]
+  print $ sumOfOddNumbsVer2 (0:[2,2,2,2,2,2,2,2,2,2,2,2,2])
+  print $ sumOfOddNumbsVer2 lista
   
 
---Not working yet
